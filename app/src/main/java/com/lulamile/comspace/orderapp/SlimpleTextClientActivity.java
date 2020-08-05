eb56f6d442581fd553866c200eaf1384d4b0f8ff
Mkhungela <mkhungela.l@gmail.com>
2020-08-05 02:06:01 +0200

    update

--- app/src/main/java/com/lulamile/comspace/orderapp/SlimpleTextClientActivity.java
+++ app/src/main/java/com/lulamile/comspace/orderapp/SlimpleTextClientActivity.java
@@ -27,41 +27,7 @@ public class SlimpleTextClientActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slimple_text_client);}
-/*
-        textField = (EditText) findViewById(R.id.table_no); // reference to the text field
-        button = (Button) findViewById(R.id.button1); // reference to the send button
 
-        // Button press event listener
-        button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                messsage = textField.getText().toString(); // get the text message on the text field
-                textField.setText(""); // Reset the text field to blank
-                SendMessage sendMessageTask = new SendMessage();
-                sendMessageTask.execute();
-            }
-        });
-    }
-
-    private class SendMessage extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-
-                client = new Socket("192.168.1.149", 4444); // connect to the server
-                printwriter = new PrintWriter(client.getOutputStream(), true);
-                printwriter.write(messsage); // write the message to output stream
-                printwriter.flush();
-                printwriter.close();
-                client.close(); // closing the connection
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
